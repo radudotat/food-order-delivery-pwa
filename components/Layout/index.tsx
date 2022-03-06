4e83fb2cfb5b680c05c8654bb833532fd4b08bcd
Radu <gith@radu.at>
2022-03-06 13:50:33 +0100

    Fixes for heroku

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import Header from "./Header";
+import React from 'react';
 
-export default function Layout(props: { children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) {
-    return (
-        <div>
-            <Header/>
-            {props.children}
-        </div>
-    );
-};
+// import Header from "./Header";
 
+export default function Layout(props: {
+  children:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}) {
+  return (
+    <div>
+      {/* <Header/> */}
+      {props.children}
+    </div>
+  );
+}
